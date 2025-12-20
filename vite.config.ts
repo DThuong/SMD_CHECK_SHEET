fc5692618fd42ca381118cdc8f394ce196dc3ff3
DESKTOP-BGE4RRB\User <hachimansuvivel@gmail.com>
2025-12-20 15:58:19 +0700

    config route server in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://172.16.162.103:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
